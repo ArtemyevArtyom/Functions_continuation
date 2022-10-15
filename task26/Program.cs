f41d099fb5af2e50aeb8b7be2c8807a3699d2ec0
ArtemyevArtyom <gof.ru@yandex.ru>
2022-10-15 23:47:35 +0400

    continuation

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+System.Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+userNumber = Math.Abs(userNumber);
+int result = 1;
+if (userNumber != 0)
+{
+    result = (int)Math.Log10(userNumber) + 1;
+}
+System.Console.WriteLine($"Количество знаков в числе {result}");
